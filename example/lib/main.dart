797fb92a445173ffc9bcc5cd3a8a0515272d15ad
Jesse Gielen <jessegielen@hotmail.com>
2021-03-04 18:02:57 +0100

    Improve example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:boustro_starter/boustro_starter.dart';
 import 'package:boustro_starter/toolbar_items.dart' as toolbar_items;
 import 'package:example/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(MyApp());
@@ -55,96 +56,102 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Boustro'),
-        actions: [
-          BoustroConfig(
-            attributeTheme: (AttributeThemeBuilder()
-                  ..boldFontWeight = FontWeight.w900)
-                .build(),
-            componentConfigData:
-                Theme.of(context).brightness == Brightness.light
-                    ? (BoustroComponentConfigBuilder()
-                          ..imageMaxHeight = 400
-                          ..imageSideColor = Colors.brown.withOpacity(0.2))
-                        .build()
-                    : (BoustroComponentConfigBuilder()
-                          ..imageMaxHeight = 350
-                          ..imageSideColor =
-                              Colors.deepPurple.shade900.withOpacity(0.2))
-                        .build(),
-            builder: (context) => IconButton(
+    return BoustroConfig(
+      attributeTheme: (AttributeThemeBuilder()
+            ..boldFontWeight = FontWeight.w900
+            ..linkOnTap = (context, url) async {
+              if (await canLaunch(url)) {
+                await launch(url);
+              }
+            })
+          .build(),
+      componentConfigData: Theme.of(context).brightness == Brightness.light
+          ? (BoustroComponentConfigBuilder()
+                ..imageMaxHeight = 400
+                ..imageSideColor = Colors.brown.withOpacity(0.2))
+              .build()
+          : (BoustroComponentConfigBuilder()
+                ..imageMaxHeight = 350
+                ..imageSideColor = Colors.deepPurple.shade900.withOpacity(0.2))
+              .build(),
+      builder: (context) => Scaffold(
+        appBar: AppBar(
+          title: Text('Boustro'),
+          actions: [
+            IconButton(
               icon: const Icon(Icons.check),
               onPressed: () {
-                Navigator.of(context)
-                    .push<void>(MaterialPageRoute<void>(builder: (context) {
-                  return Scaffold(
-                    body: DocumentView(
-                      document: controller.toDocument(),
-                    ),
-                  );
-                }));
+                Navigator.of(context).push<void>(
+                  MaterialPageRoute<void>(builder: (context) {
+                    return Scaffold(
+                      appBar: AppBar(title: Text('Preview')),
+                      body: DocumentView(
+                        document: controller.toDocument(),
+                      ),
+                    );
+                  }),
+                );
               },
             ),
-          )
-        ],
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            // Demo the auto formatter text separated with hashtags, mentions
-            // and URLs.
-            child: AutoFormatter(
-              controller: controller,
-              rules: [
-                FormatRule(CommonPatterns.hashtag, (_) => boldAttribute),
-                FormatRule(CommonPatterns.mention, (_) => italicAttribute),
-                FormatRule(CommonPatterns.httpUrl, (_) => boldAttribute),
-              ],
-              child: DocumentEditor(
+          ],
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              // Demo the auto formatter text separated with hashtags, mentions
+              // and URLs.
+              child: AutoFormatter(
                 controller: controller,
+                rules: [
+                  FormatRule(CommonPatterns.hashtag, (_) => boldAttribute),
+                  FormatRule(CommonPatterns.mention, (_) => italicAttribute),
+                  FormatRule(CommonPatterns.httpUrl, (_) => boldAttribute),
+                ],
+                child: DocumentEditor(
+                  controller: controller,
+                ),
               ),
             ),
-          ),
-          Toolbar(
-            documentController: controller,
-            defaultItemBuilder: (context, controller, item) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 2),
-                child: Center(
-                  child: IconButton(
-                    splashColor: Colors.transparent,
-                    onPressed: item.onPressed == null
-                        ? null
-                        : () => item.onPressed!(context, controller),
-                    icon: item.title!,
-                    tooltip: item.tooltip,
+            Toolbar(
+              documentController: controller,
+              defaultItemBuilder: (context, controller, item) {
+                return Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 4, horizontal: 2),
+                  child: Center(
+                    child: IconButton(
+                      splashColor: Colors.transparent,
+                      onPressed: item.onPressed == null
+                          ? null
+                          : () => item.onPressed!(context, controller),
+                      icon: item.title!,
+                      tooltip: item.tooltip,
+                    ),
                   ),
+                );
+              },
+              items: [
+                toolbar_items.bold,
+                toolbar_items.italic,
+                toolbar_items.underline,
+                toolbar_items.link(),
+                toolbar_items.title,
+                toolbar_items.image(
+                  pickImage: (_) async => NetworkImage(
+                      'https://upload.wikimedia.org/wikipedia/commons/1/19/Billy_Joel_Shankbone_NYC_2009.jpg'),
+                  snapImage: (_) async => NetworkImage(
+                      'https://upload.wikimedia.org/wikipedia/commons/1/19/Billy_Joel_Shankbone_NYC_2009.jpg'),
                 ),
-              );
-            },
-            items: [
-              toolbar_items.bold,
-              toolbar_items.italic,
-              toolbar_items.underline,
-              toolbar_items.link(),
-              toolbar_items.title,
-              toolbar_items.image(
-                pickImage: (_) async => NetworkImage(
-                    'https://upload.wikimedia.org/wikipedia/commons/1/19/Billy_Joel_Shankbone_NYC_2009.jpg'),
-                snapImage: (_) async => NetworkImage(
-                    'https://upload.wikimedia.org/wikipedia/commons/1/19/Billy_Joel_Shankbone_NYC_2009.jpg'),
-              ),
-              toolbar_items.bulletList,
-              ToolbarItem(
-                title: const Icon(Icons.wb_sunny),
-                onPressed: (context, __) =>
-                    ThemeModeScope.of(context).toggle(context),
-              ),
-            ],
-          ),
-        ],
+                toolbar_items.bulletList,
+                ToolbarItem(
+                  title: const Icon(Icons.wb_sunny),
+                  onPressed: (context, __) =>
+                      ThemeModeScope.of(context).toggle(context),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
